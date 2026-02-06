0f1598218ed6a23619c4c992d2d92233a7c65b0e
ayubnamiq13-boop <ayubnamiq13@gmail.com>
2026-02-06 12:05:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from telebot import types
 # کلیلەکانت لێرە دابنێ
 TOKEN = "8424588883:AAFxOXGpsEkQjBps9eLGAh9qSWC5JS_W-HA"
 GEMINI_API_KEY = "AIzaSyAtfMrX4eciLZmVZPbmtwk_8-ZcrGkSEzQ"
-
 try:
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel('gemini-1.5-flash')
